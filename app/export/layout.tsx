65384a215cd2cd020e8ba2589990238215a2de2e
Korawit <l3uam09@gmail.com>
2026-09-24 20:53:36 +0700

    Revert "feat: add verified dataset exports"
    This reverts commit 84c044ed1759f417c9297de5874e85fdfe7dcaeb.

--- app/export/layout.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-
-import RequireRole from '@/components/auth/RequireRole'
-
-export default function ExportLayout({ children }: { children: React.ReactNode }) {
-  return <RequireRole allowed={['admin']}>{children}</RequireRole>
-}
